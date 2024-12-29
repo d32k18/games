a3fbea56c5bfc25cce552ff3a24a31e2b14e1d76
d32k18 <190702735+d32k18@users.noreply.github.com>
2024-12-29 11:54:45 +0100

    Delete webgl8x8boardgames directory

--- webgl8x8boardgames/themes/chess/BoardCell.js
+++
@@ -1,5 +0,0 @@
-//BlenderExport = {}
-BlenderExport.BoardCell = 
-{"texCoords":[0.0,1.0,0.0,0.0,1.0,1.0,1.0,1.0,0.0,0.0,1.0,0.0],"vertices":[4.0,-4.0,-0.0,4.0,4.0,0.0,-4.0,-4.0,-0.0,-4.0,-4.0,-0.0,4.0,4.0,0.0,-4.0,4.0,0.0],"normals":[0.0,-0.0,1.0,0.0,-0.0,1.0,0.0,-0.0,1.0,0.0,-0.0,1.0,0.0,-0.0,1.0,0.0,-0.0,1.0]}
-BlenderExport.BoardCell.indices = [];
-for(var i=0;i<6;i++) BlenderExport.BoardCell.indices.push(i);
